1d4068e863a2d28d9fbf72d250058790182ff488
Manuel Chacón <mnlchcn@hotmail.es>
2018-04-23 18:47:18 +0200

    asdfadf

--- apps/configuracionGlobal.js
+++ apps/configuracionGlobal.js
@@ -1,7 +1,7 @@
 musicien.constant('configuracionGlobal' , {
-  "api_url": "http://nsd2012/Desarrollo/dsWASMusic/",
+  //"api_url": "http://nsd2012/Desarrollo/dsWASMusic/",
   //"api_url": "http://localhost:56299/",
-  //"api_url": "http://82.223.27.144/dsWASMusic/",
+  "api_url": "http://82.223.27.144/dsWASMusic/",
   //"chat_url": "http://localhost:65277/signalr",
   //"chat_url": "http://nsd2012/Desarrollo/dsSignalRChat/signalr",
   "chat_url": "http://82.223.27.144/dsSignalRChat/signalr",
